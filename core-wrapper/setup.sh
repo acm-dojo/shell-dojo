b7c70ee7f3dbe8898368d8c4cd80e206283452b0
RayZh <rayzhangshanghai@hotmail.com>
2025-09-12 11:41:56 +0800

    feat: simplify dojo creation by providing simplified api for markdown injection via wrapper

---
+++ core-wrapper/setup.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# Setup the core-wrapper environment
+chown root:root /mnt/core-wrapper/wrapper
+chmod 4755 /mnt/core-wrapper/wrapper
+ln -s /mnt/core-wrapper/wrapper /tutorial
+
+# Setup bashrc to source core-wrapper bashrc on user login
+echo 'source /core-wrapper/.bashrc' >> /etc/bash.bashrc
+ln -s /etc/bash.bashrc /etc/bashrc
