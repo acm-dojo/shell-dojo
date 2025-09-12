b6728c7c9de74286f8a24f868a426b8b7f12b0fa
RayZh <rayzhangshanghai@hotmail.com>
2025-09-12 11:46:30 +0800

    fix: typo in path for .bashrc

--- core-wrapper/setup.sh
+++ core-wrapper/setup.sh
@@ -6,5 +6,5 @@ chmod 4755 /mnt/core-wrapper/wrapper
 ln -s /mnt/core-wrapper/wrapper /tutorial
 
 # Setup bashrc to source core-wrapper bashrc on user login
-echo 'source /core-wrapper/.bashrc' >> /etc/bash.bashrc
+echo 'source /mnt/core-wrapper/.bashrc' >> /etc/bash.bashrc
 ln -s /etc/bash.bashrc /etc/bashrc
