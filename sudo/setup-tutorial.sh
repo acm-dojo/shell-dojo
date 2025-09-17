15b5d00f18bf1a9aae98b287f393bf015f599b50
RayZh <rayzhangshanghai@hotmail.com>
2025-09-17 08:45:24 +0800

    feat: add card support for /mnt/sudo

--- sudo/setup-tutorial.sh
+++ sudo/setup-tutorial.sh
@@ -2,7 +2,11 @@
 
 # Sign the tutorial script
 echo "/mnt/sudo/run-tutorial" >> /challenge/.signature
+echo "/mnt/sudo/run-card" >> /challenge/.signature
+
+# Alias for users
 ln -s /mnt/sudo/run-tutorial /challenge/tutorial
+ln -s /mnt/sudo/run-card /challenge/card
 
 # Setup bashrc to run tutorial on user login
 echo '/mnt/sudo/run-tutorial' >> /etc/bash.bashrc
